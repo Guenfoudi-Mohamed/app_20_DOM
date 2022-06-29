fa8ce2887ce4d9f7a71346f32817e3b07f8f6f50
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-06-29 20:55:25 +0100

    Update app_21.js

--- app_21.js
+++ app_21.js
@@ -1,6 +1,6 @@
 // app_21
 
-
+// FUNCTION FOR ANIMATION
 function setRnadomShape(){
     const shape = document.createElement('div');
     shape.setAttribute('class','shape');
@@ -8,7 +8,7 @@ function setRnadomShape(){
     let stockHeight = 0;
     let stockLeft = 0;
     let stockAnimationDuration = 0;
-    console.log(shape);
+    // console.log(shape);
     // let stockAnimationDelay;
     function width(){
         stockWidth = Math.ceil(Math.random() * 3);
@@ -32,8 +32,8 @@ function setRnadomShape(){
     }
     height();
     function left(){
-        stockLeft = Math.ceil(Math.random() * 95);
-        if(stockLeft >= 1 && stockLeft <= 95){
+        stockLeft = Math.ceil(Math.random() * 98);
+        if(stockLeft >= 2 && stockLeft <= 98){
             shape.style.setProperty('left',`${stockLeft}vw`);
         }
         else{
@@ -61,11 +61,63 @@ function setRnadomShape(){
         }
     }
     animationDelay();
-    document.body.appendChild(shape);
+    document.querySelector('body #shapes').appendChild(shape);
 }
-let times = 50;
+// RUN THE FUNCTION 
+let times = 75;
 while(times>0){
     times--;
     setRnadomShape();
 
 };
+
+//  === 
+
+const inpPerspective = document.querySelector('body .container .inputControles > .inpPerspective');
+const inpRotateX = document.querySelector('body .container .inputControles > .inpRotateX');
+const inpRotateY = document.querySelector('body .container .inputControles > .inpRotateY');
+const inpRotateZ = document.querySelector('body .container .inputControles > .inpRotateZ');
+const box = document.querySelector('body .container .boxTest .box');
+inpPerspective.oninput = function(){
+    box.parentElement.style.setProperty('perspective',`${inpPerspective.value}px`);
+    document.querySelector('body .container .inputControles .lblPerspective span').textContent = `${inpPerspective.value}px`;
+};
+inpRotateX.oninput = function(){
+    box.style.setProperty('transform',`rotateX(${inpRotateX.value}deg) rotateY(${inpRotateY.value}deg) rotateZ(${inpRotateZ.value}deg)`);
+    document.querySelector('body .container .inputControles .lblRotateX span').textContent = `${inpRotateX.value}deg`;
+
+};
+inpRotateY.oninput = function(){
+    box.style.setProperty('transform',`rotateX(${inpRotateX.value}deg) rotateY(${inpRotateY.value}deg) rotateZ(${inpRotateZ.value}deg)`);
+    document.querySelector('body .container .inputControles .lblRotateY span').textContent = `${inpRotateY.value}deg`;
+
+};
+inpRotateZ.oninput = function(){
+    box.style.setProperty('transform',`rotateX(${inpRotateX.value}deg) rotateY(${inpRotateY.value}deg) rotateZ(${inpRotateZ.value}deg)`);
+    document.querySelector('body .container .inputControles .lblRotateZ span').textContent = `${inpRotateZ.value}deg`;
+
+};
+
+const btnRest = document.querySelector('body .container .inputControles .btns .reset');
+btnRest.onclick = function(){
+    inpPerspective.value = 250;
+    inpPerspective.oninput();
+    inpRotateX.value = 0;
+    inpRotateX.oninput();
+    inpRotateY.value = 0;
+    inpRotateY.oninput();
+    inpRotateZ.value = 0;
+    inpRotateZ.oninput();
+}
+
+const btncopy = document.querySelector('body .container .inputControles .btns .copy');
+btncopy.onclick = function(){
+    const copyCodeCss = `perspective: ${inpPerspective.value}px;transform: rotateX(${inpRotateX.value}deg) rotateY(${inpRotateY.value}deg) rotateZ(${inpRotateZ.value}deg);`;
+    // copyCodeCss.select();
+    navigator.clipboard.writeText(copyCodeCss); 
+    // console.log(`perspective: ${inpPerspective.value}px;transform: rotateX(${inpRotateX.value}deg) rotateY(${inpRotateY.value}deg) rotateZ(${inpRotateZ.value}deg);`);
+};
+
+window.onload = function(){
+    btnRest.click();
+}
